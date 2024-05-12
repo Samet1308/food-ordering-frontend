f5ed1cf4f6032a619222b828d3d3b0d2983d0572
Samet Kayrak <abdus.kayrak@gmail.com>
2024-05-12 12:36:09 +0300

    Food Ordering Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,51 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule, provideHttpClient} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth-components/signup/signup.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzGridModule} from "ng-zorro-antd/grid";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzSpinModule} from "ng-zorro-antd/spin";
+import {NzWaveModule} from "ng-zorro-antd/core/wave";
+
+import {en_US, NZ_I18N} from "ng-zorro-antd/i18n";
+import {NzHeaderComponent} from "ng-zorro-antd/layout";
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './auth-components/login/login.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzFormModule,
+    NzGridModule,
+    NzInputModule,
+    NzSpinModule,
+    NzWaveModule,
+    NzHeaderComponent,
+
   ],
   providers: [
-    provideClientHydration()
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
